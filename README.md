49e13cef7cbb960aa9152a710c3ee6436262b3f1
Julian Pedraza <pedrazajulian@hotmail.com>
2021-06-29 21:11:43 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# ReadmeGen
+# ⚡ASDASD⚡
+
+  ![Github License](https://img.shields.io/badge/license-MIT,Apache,GPL,none-blue.svg)
+  
+
+  ## Description
+  ASDASDAS
+  
+  <ur>
+  
+  ## 📋Table of Contents
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  
+[License](#license)
+
+  
+  [Contributing](#contributing)
+
+  [Test](#test)
+  
+  [Questions](#questions)
+
+  
+  ## 💾Installation  
+  ASDASD
+
+  <ur>
+
+  ## 💻 Usage  
+  ASASD
+  ## License 
+    This project is licensed with MIT,Apache,GPL,none.
+
+  <ur>
+
+  ## 👪 Contributing  
+  ASDASD
+  <ur>
+
+  ## ✏️ Test 
+  ASDASD
+  <ur>
+
+  ## ✋ Questions 
+  Any questions? Contact ASDASD. Access more of my work at [ASDASD](https://github.com/ASDASD/).
---
+++ index.js
@@ -0,0 +1,69 @@
+
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        }, {
+            type: 'input',
+            message: 'Describe the application.',
+            name: 'description',
+        },{
+            type: 'input',
+            message: 'What would this site be used for?',
+            name: 'usage',
+        }, {
+            type: 'input',
+            message: 'How do you install the application?',
+            name: 'install',
+        }, {
+            type: 'input',
+            message: 'List the collaborators.',
+            name: 'collaborators',
+        }, { 
+            type: 'checkbox',
+            message: 'Which licenses were used?',
+            name: 'license',
+            choices: ['MIT', 'Apache', 'GPL', 'none'],
+        }, {
+            type:'input',
+            message:'What are the contribution guidelines?',
+            name:'contribution'
+        }, {
+            type: 'input',
+            message: 'How do you test the application?',
+            name: 'test',
+            default: 'npm test',
+        },{
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username',
+        }, {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        }, 
+    ];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+};
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('Generating ReadMe...');
+        writeToFile('README.md', generateMarkdown({...inquirerResponses}));
+    });
+
+};
+// Function call to initialize app
+init();
